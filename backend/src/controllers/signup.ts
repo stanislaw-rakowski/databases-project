a7e13c211d7119a5286a33e74749ff45c131c80a
stanislaw-rakowski <stasiu.rakowski@gmail.com>
2022-10-26 23:42:07 +0200

    check if email already taken

--- backend/src/controllers/signup.ts
+++ backend/src/controllers/signup.ts
@@ -8,6 +8,18 @@ export const SignupController = (server: FastifyInstance) => ({
 		try {
 			const { email, password } = request.body
 
+			const [organizations] = (await server.mysql.query('SELECT * FROM `Organizations` WHERE `email` = ?', [
+				email,
+			])) as [Organization[], any]
+
+			if (organizations.length > 0) {
+				reply.status(401)
+
+				return {
+					message: `Organization with email ${email} is already registered`,
+				}
+			}
+
 			const hashedPassword = await hash(password, 10)
 
 			const organizationId = uuid()
