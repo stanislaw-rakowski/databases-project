19b0d2646cdffd71577c6155bcfa6586d1a66ad6
stanislaw-rakowski <stasiu.rakowski@gmail.com>
2022-12-03 13:19:19 +0100

    add api public routes

---
+++ backend/src/controllers/public.ts
@@ -0,0 +1,50 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+import { Animal } from '../schemas/animal'
+
+export const PublicController = (server: FastifyInstance) => ({
+	async getAnimals(request: FastifyRequest, reply: FastifyReply) {
+		try {
+			const [results] = (await server.mysql.query(`
+                SELECT * 
+                FROM Animals 
+                WHERE shelter IN (
+                    SELECT shelter
+                    FROM Shelters
+                    WHERE published = true
+                )
+            `)) as [Animal[], unknown]
+
+			reply.status(200)
+
+			return results
+		} catch (error) {
+			reply.status(500)
+
+			return {
+				message: 'Failed to get public animals',
+				error,
+			}
+		}
+	},
+
+	async getShelters(request: FastifyRequest, reply: FastifyReply) {
+		try {
+			const [results] = (await server.mysql.query(`
+                SELECT *
+                FROM Shelters
+                WHERE published = true
+            `)) as [Animal[], unknown]
+
+			reply.status(200)
+
+			return results
+		} catch (error) {
+			reply.status(500)
+
+			return {
+				message: 'Failed to get public shelters',
+				error,
+			}
+		}
+	},
+})
