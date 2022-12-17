6eb1d824e9f5034c83f396c0d62a40b2aef1b227
stanislaw-rakowski <stasiu.rakowski@gmail.com>
2022-12-18 00:06:16 +0100

    improve account page

--- frontend/src/pages/ManagementApp/AccountPage.tsx
+++ frontend/src/pages/ManagementApp/AccountPage.tsx
@@ -1,15 +1,51 @@
 import React from 'react'
+import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom'
+import { FaUser } from 'react-icons/fa'
 import { AppWrapper, AppContent } from '../../components/common'
-import { removeAuth } from '../../lib/auth'
+import { removeAuth, getAuth } from '../../lib/auth'
 import { deleteAccount, seedAccountDatabase } from '../../lib/api'
 import SideBarMenu from '../../components/SideBarMenu'
 import Button from '../../components/Button'
 import ActionModal from '../../components/ActionModal'
+import Toast from '../../components/Toast'
+
+const Content = styled.div`
+	display: flex;
+	justify-content: center;
+	gap: 50px;
+	align-items: flex-start;
+	margin-top: 30px;
+`
+
+const Avatar = styled.div`
+	height: 200px;
+	width: 200px;
+	border-radius: 50%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	font-size: 100px;
+	background-color: ${({ theme }) => theme.colors.secondaryBackground};
+`
+
+const ButtonsSection = styled.div`
+	display: flex;
+	width: 100%;
+	margin-top: 20px;
+	justify-content: center;
+	gap: 20px;
+`
+
+const Title = styled.p`
+	font-size: 1.3rem;
+`
 
 const AccountPage = () => {
-	const [showDeleteAccountModal, setShowDeleteAccountModal] = React.useState(false)
 	const navigate = useNavigate()
+	const [showDeleteAccountModal, setShowDeleteAccountModal] = React.useState(false)
+	const [error, setError] = React.useState(false)
+	const data = getAuth()
 
 	const handleLogOut = () => {
 		removeAuth()
@@ -21,8 +57,8 @@ const AccountPage = () => {
 			await deleteAccount()
 			removeAuth()
 			navigate('/about')
-		} catch (error) {
-			console.log(error)
+		} catch {
+			setError(true)
 		}
 	}
 
@@ -30,16 +66,32 @@ const AccountPage = () => {
 		<AppWrapper>
 			<SideBarMenu />
 			<AppContent>
-				<h1>Account</h1>
-				<Button variant="frame" onClick={handleLogOut}>
-					Log out
-				</Button>
-				<Button variant="secondary" onClick={seedAccountDatabase}>
-					Seed database
-				</Button>
-				<Button variant="destructive" onClick={() => setShowDeleteAccountModal(true)}>
-					Delete account
-				</Button>
+				<h1>Manage your Account</h1>
+				<Content>
+					<Avatar>
+						<FaUser />
+					</Avatar>
+					<div>
+						<Title>Email:</Title>
+						<p>{data?.email}</p>
+						<Title>Organization ID:</Title>
+						<p>{data?.organizationId}</p>
+					</div>
+				</Content>
+				<Content>
+					<ButtonsSection>
+						<Button variant="frame" onClick={handleLogOut}>
+							Log out
+						</Button>
+						<Button variant="secondary" onClick={seedAccountDatabase}>
+							Seed database
+						</Button>
+						<Button variant="destructive" onClick={() => setShowDeleteAccountModal(true)}>
+							Delete account
+						</Button>
+					</ButtonsSection>
+				</Content>
+				{error && <Toast variant="error" message="Something went wrong" />}
 				{showDeleteAccountModal && (
 					<ActionModal
 						text="Are you sure?"
