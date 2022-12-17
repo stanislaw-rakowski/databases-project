8432fd3d1e15ee9c14a9a483908546bde1ccfad7
stanislaw-rakowski <stasiu.rakowski@gmail.com>
2022-12-17 22:45:51 +0100

    remove cookies package

--- backend/src/plugins/cookies.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from 'fastify'
-import fp from 'fastify-plugin'
-import cookie, { FastifyCookieOptions } from '@fastify/cookie'
-
-async function Cookies(server: FastifyInstance) {
-	server.register<FastifyCookieOptions>(cookie)
-}
-
-export default fp(Cookies)
